2948f4198208113c1473b1f2ab1362b6a3c8f562
Hong Phuc <hongphuc.dthp@gmail.com>
2020-06-13 20:51:19 +0700

    toggle nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../logo.svg";
 import "./Header.css";
 
 export default function Header() {
+	const [isNavVisible, setIsNavVisible] = useState(false);
+	const toggleNav = () => setIsNavVisible(!isNavVisible);
+
 	return (
 		<header className="Header">
 			<img src={logo} alt="logo" className="Logo" />
-			<nav className="Nav">
-				<a href="/">Home</a>
-				<a href="/">Articles</a>
-				<a href="/">About</a>
-				<button>Logout</button>
-			</nav>
+			{isNavVisible && (
+				<nav className="Nav">
+					<a href="/">Home</a>
+					<a href="/">Articles</a>
+					<a href="/">About</a>
+					<button>Logout</button>
+				</nav>
+			)}
 			{/* eslint-disable-next-line jsx-a11y/accessible-emoji  */}
-			<button className="Burger">🍔</button>
+			<button className="Burger" onClick={toggleNav}>
+				🍔
+			</button>
 		</header>
 	);
 }
