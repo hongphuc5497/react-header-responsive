19bcc8d33a3b9ab7722d3fc99a1bb51841abc7bf
Hong Phuc <hongphuc.dthp@gmail.com>
2020-06-13 21:29:05 +0700

    Handle transition between small screen and large screen

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import logo from "../logo.svg";
 import "./Header.css";
 
 export default function Header() {
-	const [isNavVisible, setIsNavVisible] = useState(false);
-	const toggleNav = () => setIsNavVisible(!isNavVisible);
+	const [isNavVisible, setNavVisibility] = useState(false);
+	const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+	const toggleNav = () => setNavVisibility(!isNavVisible);
+
+	const handleMediaQueryChange = (mediaQuery) => {
+		console.log(mediaQuery);
+		if (mediaQuery.matches) {
+			setIsSmallScreen(true);
+		} else {
+			setIsSmallScreen(false);
+		}
+	};
+
+	useEffect(() => {
+		const mediaQuery = window.matchMedia('(max-width: 700px)');
+		mediaQuery.addListener(handleMediaQueryChange);
+		handleMediaQueryChange(mediaQuery);
+
+		return () => {
+			mediaQuery.removeListener(handleMediaQueryChange);
+		};
+	}, []);
 
 	return (
 		<header className="Header">
 			<img src={logo} alt="logo" className="Logo" />
-			{isNavVisible && (
+			{(!isSmallScreen || isNavVisible) && (
 				<nav className="Nav">
 					<a href="/">Home</a>
 					<a href="/">Articles</a>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './style.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./style.css";
 import Header from "./components/Header";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Header />
-    <div className="Content">
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita, voluptate? Minus soluta laudantium debitis. Totam ipsam, non accusantium optio voluptates cumque nulla enim eum sint perferendis aperiam odio! Neque, accusamus!</p>
-    </div>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<div className="App">
+			<Header />
+			<div className="Content">
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita,
+					voluptate? Minus soluta laudantium debitis. Totam ipsam, non
+					accusantium optio voluptates cumque nulla enim eum sint perferendis
+					aperiam odio! Neque, accusamus!
+				</p>
+			</div>
+		</div>
+	</React.StrictMode>,
+	document.getElementById("root")
 );
